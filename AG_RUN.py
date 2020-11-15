1b9225d136c761daef26bb5674192155d058771f
MAXIORBOY <michal.grzegorz.sobieraj@gmail.com>
2020-11-15 09:28:58 +0100

    Removed script launchers

--- AG_RUN.py
+++
@@ -1,4 +0,0 @@
-from artificial_generator import ArtificialGenerator
-
-if __name__ == '__main__':
-    ArtificialGenerator('sphere').generate_flights()
--- RUN.py
+++
@@ -1,4 +0,0 @@
-from main import ShapeEstimator
-
-if __name__ == '__main__':
-    ShapeEstimator('flights.csv', point_details_file_name='airports.csv', color_definitions_file='color_definitions.csv', optimized_points_file='earth.pickle').draw_plot()
